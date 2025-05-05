7706ea3c17dff162dd258106ab6c65dbe813f786
Clebert Civil <clebertcivil01@gmail.com>
2025-05-05 03:20:50 -0400

    add jquery to my project

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active")
 })
 
-document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click", () =>{
+document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
