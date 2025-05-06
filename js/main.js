c9ea0cbb8c4fc31aba13439df36bed53c6ad2fbc
Clebert Civil <clebertcivil01@gmail.com>
2025-05-06 03:46:53 -0400

    modify my input text to input search and add the structure for for ajax to call my api

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click",
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+$(document).ready(() => {
+    const btnSubmit = $('button[type=submit]');
+    const apiKey = '3hUBiuIT4SovDhQ3P6ATaDnIuVLGpXhz';
+    const limit = 9;
+    btnSubmit.click(() => {
+        const searchText = $('#inp-search').val().trim();
+        console.log(searchText)
+    })
+})
