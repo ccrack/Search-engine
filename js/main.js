20e9f1a67befa0a719719eea08f24939b6cf2207
ccrack <clebertcivil01@gmail.com>
2025-05-17 04:53:18 -0400

    add listener on each git to show that gif on the top of page

--- js/main.js
+++ js/main.js
@@ -2,53 +2,69 @@ let hamburger = document.querySelector(".hamburger");
 let navMenu = document.querySelector(".menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".menu-item").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+  })
+);
 $(document).ready(() => {
-    const btnSubmit = $('button[type=submit]');
-    const apiKey = '3hUBiuIT4SovDhQ3P6ATaDnIuVLGpXhz';
-    const limit = 18;
-    btnSubmit.click(() => {
-        const searchText = $('#inp-search').val().trim();
-        getData(searchText)
-    })
-    getData("happy")
-    function getData(txtSearch){
-        $.ajax({
-            url: `https://api.giphy.com/v1/gifs/search`,
-            method: 'GET',
-            data: {
-                api_key: apiKey,
-                q: txtSearch,
-                limit: limit
-            },
-            success: ((response) => {
-                const gifsContainer = $(".gifs-container");
-                gifsContainer.empty();
-                /* loop my data to get each gif url*/
-                response.data.forEach(gif => {
-                    /* get my gif url (getUrlgif) */
-                    const getUrlGif = gif.images.fixed_width.url;
-                    const elementGif = `
+  const btnSubmit = $("button[type=submit]");
+  const apiKey = "3hUBiuIT4SovDhQ3P6ATaDnIuVLGpXhz";
+  const limit = 18;
+  btnSubmit.click(() => {
+    const searchText = $("#inp-search").val().trim();
+    document.querySelector(".targetGif-container").removeChild(document.querySelector("img"));
+    getData(searchText);
+  });
+
+  getData("happy");
+
+  function getData(txtSearch) {
+    $.ajax({
+      url: "https://api.giphy.com/v1/gifs/search",
+      method: "GET",
+      data: {
+        api_key: apiKey,
+        q: txtSearch,
+        limit: limit,
+      },
+      success: (response) => {
+        const gifsContainer = $(".gifs-container");
+        gifsContainer.empty();
+        /* loop my data to get each gif url*/
+        response.data.forEach((gif) => {
+          /* get my gif url (getUrlgif) */
+          const getUrlGif = gif.images.fixed_width.url;
+          const elementGif = `
                         <div class = "gif-item">
                             <img src="${getUrlGif}" alt="${txtSearch}">
                         </div>
-                    `
-                    gifsContainer.append(elementGif)
-                    
-                    console.log(elementGif);
-                })
-            }),
-            error: ((error) => {
-                console.log("error: " + error)
-            })
-        })
-        console.log(txtSearch)
-    }
-})
+                    `;
+          gifsContainer.append(elementGif);
+        });
+
+        const gifs = document.querySelectorAll("img[src]");
+        const targetContainer = document.querySelector(".targetGif-container");
+        const img = document.createElement("img");
+        gifs.forEach((gifTarget) => {
+          gifTarget.addEventListener("click", (e) => {
+            const gifLink = e.target.src;
+            if (gifLink !== null) {
+              img.setAttribute("src", gifLink);
+              img.setAttribute("alt", e.target.alt);
+              targetContainer.append(img);
+            }
+          });
+        });
+      },
+      error: (error) => {
+        console.log("error: " + error);
+      },
+    });
+  }
+});
